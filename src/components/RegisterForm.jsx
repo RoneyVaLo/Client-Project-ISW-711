d29cf9e06b0555038a29ad0a2daabe9c57e97ec6
Roney Valdelomar <rvaldelomarlopez@gmail.com>
2023-07-09 03:54:09 -0600

    create form for register page

---
+++ src/components/RegisterForm.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+
+const RegisterForm = () => {
+
+    const [showPassword, setShowPassword] = useState(false);
+
+    const [showRepeatPassword, setShowRepeatPassword] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h1>User Registration</h1>
+
+            <div>
+                <label htmlFor="email">Email</label>
+                <input type="email" name="email" required />
+            </div>
+
+            <div>
+                <label htmlFor="firstName">First Name</label>
+                <input type="text" name="firstName" required />
+            </div>
+
+            <div>
+                <label htmlFor="lastName">Last Name</label>
+                <input type="text" name="lastName" required />
+            </div>
+
+            <div>
+                <label htmlFor="password">Password</label>
+                <input type={showPassword ? "text" : "password"} name="password" required autoComplete="off" />
+                <div>
+                    <input type="checkbox" name="showPassword"
+                        onChange={(e) => setShowPassword(e.target.checked)}
+                    />
+                    <label htmlFor="showPassword">Show Password</label>
+                </div>
+            </div>
+
+            <div>
+                <label htmlFor="repeatPassword">Password</label>
+                <input type={showRepeatPassword ? "text" : "password"} name="repeatPassword" autoComplete="off" />
+                <div>
+                    <input type="checkbox" name="showRepeatPassword"
+                        onChange={(e) => setShowRepeatPassword(e.target.checked)}
+                    />
+                    <label htmlFor="showRepeatPassword">Show Repeated Password</label>
+                </div>
+            </div>
+
+            <div>
+                <button>Register</button>
+                <button>Cancel</button>
+            </div>
+        </form>
+    );
+}
+
+export default RegisterForm;
