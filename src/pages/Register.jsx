c5d3eeda49efaffd97e2f2d36329972316ea2496
Roney Valdelomar <rvaldelomarlopez@gmail.com>
2023-07-09 03:53:34 -0600

    implement the component with the singup form

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,8 @@
+import RegisterForm from "../components/RegisterForm";
+
 const Register = () => {
     return (
-        <div>
-            REGISTER
-        </div>
+        <RegisterForm />
     );
 }
 
