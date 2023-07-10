251ae9dcd02b75bfa11017ada7c4bdc373df9ce0
Roney Valdelomar <rvaldelomarlopez@gmail.com>
2023-07-10 01:03:26 -0600

    update components routes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Header';
-import Navigation from "../components/Navigation";
+import Navigation from "../components/Navigation/Navigation";
 
 const Home = () => {
     return (
         <>
-        <Header />
         <Navigation />
+        <Header />
         <Outlet />
         </>
     );
