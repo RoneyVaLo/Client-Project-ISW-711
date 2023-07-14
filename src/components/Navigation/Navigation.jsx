5bfe8a028cba207c8f6ab43752c486badff30b28
Roney Valdelomar <rvaldelomarlopez@gmail.com>
2023-07-14 03:13:57 -0600

    add validation to only show home tab in navbar if not logged in

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,17 +2,20 @@ import { useNavigate } from "react-router-dom";
 
 import './navigation.scss';
 import { useEffect } from "react";
+import { useAuth } from "../../context/AuthContext";
 
 const Navigation = () => {
 
     const navigate = useNavigate();
+
+    const auth = useAuth();
+    // console.log(auth.currentUser === "");
 
     useEffect(() => {
         document.getElementById('home').checked = true;
     }, [])
 
 
-    // TODO: Hacer que solo aparezca el home cuando no se esté logueado
     // TODO: Cambiar las rutas a las demás secciones una vez logueado
 
     return (
@@ -28,23 +31,27 @@ const Navigation = () => {
                     </label>
                 </div>
 
-                <div className="menu__link">
-                    <input type="radio" name="links" id="prompts"
-                        onChange={(e) => ((e.target.checked) && navigate("/"))}
-                    />
-                    <label htmlFor="prompts">
-                        <ion-icon name="images-sharp"></ion-icon>
-                    </label>
-                </div>
+                {auth.currentUser !== "" &&
+                    <>
+                        <div className="menu__link">
+                            <input type="radio" name="links" id="prompts"
+                                onChange={(e) => ((e.target.checked) && navigate("/"))}
+                            />
+                            <label htmlFor="prompts">
+                                <ion-icon name="images-sharp"></ion-icon>
+                            </label>
+                        </div>
 
-                <div className="menu__link">
-                    <input type="radio" name="links" id="users"
-                        onChange={(e) => ((e.target.checked) && navigate("/"))}
-                    />
-                    <label htmlFor="users">
-                        <ion-icon name="people-sharp"></ion-icon>
-                    </label>
-                </div>
+                        <div className="menu__link">
+                            <input type="radio" name="links" id="users"
+                                onChange={(e) => ((e.target.checked) && navigate("/"))}
+                            />
+                            <label htmlFor="users">
+                                <ion-icon name="people-sharp"></ion-icon>
+                            </label>
+                        </div>
+                    </>
+                }
             </div>
         </nav>
     );
