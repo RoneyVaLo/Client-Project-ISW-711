dd82b43bf3ee2932cc3a83957647d960df73e35b
Roney Valdelomar <rvaldelomarlopez@gmail.com>
2023-07-27 23:29:52 -0600

    update to show users

--- src/components/DataTable/DataTable.jsx
+++ src/components/DataTable/DataTable.jsx
@@ -18,17 +18,18 @@ const DataTable = ({ headers, data, handleRun, handleEdit, handleDelete }) => {
             </thead>
 
             <tbody>
+
                 {data.map((fact, index) => (
                     <tr key={index}>
                         <td>
                             <input type="checkbox" name="" id="" />
                         </td>
-                        <td>{fact.name}</td>
+                        <td>{fact[fact.name ? "name" : "first_name"]}</td>
                         <td>{fact[(fact.type ? "type" : "email")]}</td>
-                        <td>{fact[(fact.tags ? "tags" : "status")]}</td>
+                        <td>{fact.tags ? fact.tags : `${fact.verified}`}</td>
 
                         <td className='crud'>
-                            <button onClick={() => handleRun(fact._id)}><ion-icon name="eye-sharp"></ion-icon></button>
+                            <button onClick={() => {handleRun && handleRun(fact._id)}}><ion-icon name="eye-sharp"></ion-icon></button>
                             <button onClick={() => handleEdit(fact._id)}><ion-icon name="pencil-sharp"></ion-icon></button>
                             <button onClick={() => handleDelete(fact._id)}><ion-icon name="trash-sharp"></ion-icon></button>
                         </td>
